4fa024bb2deb6d7c3fee67e1a62518d5960f73f1
SirVoRaK <sirvoraky@gmail.com>
2021-12-21 02:54:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Powercord GET IP
---
+++ index.js
@@ -0,0 +1,27 @@
+const { Plugin } = require('powercord/entities');
+
+module.exports = class GetIP extends Plugin {
+  startPlugin() {
+    console.log("Get IP plugin loaded!")
+    powercord.api.commands.registerCommand({
+        command: 'ip',
+        aliases: [],
+        description: 'Returns your public IP.',
+        usage: '{c} [--send]',
+        executor: async (args) => ({send: args.includes('--send'), result: "Public IP: " + await this.ip()})
+    })
+  }
+
+  pluginWillUnload() {
+    powercord.api.commands.unregisterCommand('ip')
+  }
+
+  async ip() {
+    try{
+      const response = await fetch("https://ifconfig.me/ip")
+      return await response.text()
+    }catch(e){
+      return "Try again"
+    }
+  }
+}
